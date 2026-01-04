d3fa267f388427917f8493bc2fff52fd40fba272
Swapnil Petkar <swapnilpetkar7@gmail.com>
2026-01-04 12:51:58 +0530

    Update opencvtt.py

--- opencvtt.py
+++ opencvtt.py
@@ -1,14 +1,1 @@
-# test_opencv.py
-import cv2
-import mediapipe as mp
 
-print("OpenCV version:", cv2.__version__)
-print("MediaPipe version:", mp.__version__)
-
-# Test webcam
-cap = cv2.VideoCapture(0)
-if cap.isOpened():
-    print("Webcam working!")
-    cap.release()
-else:
-    print("Webcam not available")
